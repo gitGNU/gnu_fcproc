90ca046060bc008fbd895f5a8fc78b06b9903960
Francesco Salvestrini <salvestrini@gmail.com>
2008-09-17 20:28:39 +0200

    Fixed compilation issues

--- src/main.cxx
+++ src/main.cxx
@@ -76,10 +76,9 @@ bool transform(const std::string & input)
 int main(int argc, char * argv[])
 {
 	int c;
-	int digit_optind = 0;
-
+	// int digit_optind = 0;
 	while (1) {
-		int this_option_optind = optind ? optind : 1;
+		// int this_option_optind = optind ? optind : 1;
 		int option_index       = 0;
 
 		static struct option long_options[] = {
