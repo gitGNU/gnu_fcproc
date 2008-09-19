1e6f395ee41b1489b76fa5eef05b8590aadb3467
Francesco Salvestrini <salvestrini@gmail.com>
2008-09-19 23:10:33 +0200

    Fixed config.h wrapper

--- src/config.h
+++ src/config.h
@@ -1,63 +1,29 @@
-/* src/config.h.  Generated from config.h.in by configure.  */
-/* src/config.h.in.  Generated from configure.ac by autoheader.  */
+//
+// Copyright (C) 2008 Francesco Salvestrini
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
 
-/* Define to 1 if your C++ compiler doesn't accept -c and -o together. */
-/* #undef CXX_NO_MINUS_C_MINUS_O */
+#ifndef CONFIG_H
+#define CONFIG_H
 
-/* Define to 1 if you have the <execinfo.h> header file. */
-#define HAVE_EXECINFO_H 1
+#include "autoconfig.h"
 
-/* Define to 1 if you have the <inttypes.h> header file. */
-#define HAVE_INTTYPES_H 1
+// Checks for prerequisites
+#ifndef HAVE_UNISTD_H
+#error "Your system lacks of unistd.h support"
+#endif
 
-/* Define to 1 if you have the <memory.h> header file. */
-#define HAVE_MEMORY_H 1
-
-/* Define to 1 if you have the <stdint.h> header file. */
-#define HAVE_STDINT_H 1
-
-/* Define to 1 if you have the <stdlib.h> header file. */
-#define HAVE_STDLIB_H 1
-
-/* Define to 1 if you have the <strings.h> header file. */
-#define HAVE_STRINGS_H 1
-
-/* Define to 1 if you have the <string.h> header file. */
-#define HAVE_STRING_H 1
-
-/* Define to 1 if you have the <sys/stat.h> header file. */
-#define HAVE_SYS_STAT_H 1
-
-/* Define to 1 if you have the <sys/types.h> header file. */
-#define HAVE_SYS_TYPES_H 1
-
-/* Define to 1 if you have the <unistd.h> header file. */
-#define HAVE_UNISTD_H 1
-
-/* Name of package */
-#define PACKAGE "fcp"
-
-/* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "salvestrini@gmail.com"
-
-/* Define to the full name of this package. */
-#define PACKAGE_NAME "Filter Chains Processor"
-
-/* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Filter Chains Processor 0.0.0"
-
-/* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "fcp"
-
-/* Define to the version of this package. */
-#define PACKAGE_VERSION "0.0.0"
-
-/* Define to 1 if you have the ANSI C header files. */
-#define STDC_HEADERS 1
-
-/* Version number of package */
-#define VERSION "0.0.0"
-
-/* Define to 1 if `lex' declares `yytext' as a `char *' by default, not a
-   `char[]'. */
-#define YYTEXT_POINTER 1
+#endif // CONFIG_H
