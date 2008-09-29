65774f5a0e22586540d38ac764daeb9ae085cef7
Francesco Salvestrini <salvestrini@gmail.com>
2008-09-29 21:26:14 +0200

    Added filter related sources

---
+++ src/filter.cxx
@@ -0,0 +1,160 @@
+//
+// Copyright (C) 2007, 2008 Francesco Salvestrini
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#include "config.h"
+
+#include <string>
+#include <vector>
+
+#include "libs/misc/trace.h"
+#include "libs/misc/debug.h"
+#include "filter.h"
+
+using namespace Graph;
+
+Rule11::Rule11(const std::string & command) :
+	command_(command)
+{
+}
+
+Rule11::~Rule11(void)
+{
+}
+
+bool Rule11::run(std::string & input,
+		 std::string & output)
+{
+	TR_DBG("Running command '%s' with input '%s' and output '%s'\n",
+	       command_.c_str(), input.c_str(), output.c_str());
+
+	return true;
+}
+
+Node::Node(const std::string & tag,
+	   const std::string & command) :
+	Rule11(command),
+	tag_(tag)
+{
+	TR_DBG("Node %p created\n", this);
+}
+
+Node::~Node(void)
+{
+	TR_DBG("Node %p destroyed\n", this);
+}
+
+const std::string & Node::tag(void)
+{
+	return tag_;
+}
+
+Tree::Tree(const std::string & tag,
+	   const std::string & command) :
+	Node(tag, command)
+{
+	father_ = 0;
+	children_.clear();
+
+	TR_DBG("Tree %p created\n", this);
+}
+
+Tree::~Tree(void)
+{
+	TR_DBG("Tree %p destroyed\n", this);
+
+}
+
+void Tree::father(Tree * tree)
+{
+	father_ = tree;
+}
+
+Tree * Tree::father(void)
+{
+	return father_;
+}
+
+void Tree::child(Tree * tree)
+{
+}
+
+const std::vector<Tree *> & Tree::children(void)
+{
+	return children_;
+}
+
+DAG::DAG(void)
+{
+	root_ = 0;
+
+	TR_DBG("DAG %p created\n", this);
+}
+
+DAG::~DAG(void)
+{
+	if (root_) {
+		delete root_;
+	}
+
+	TR_DBG("DAG %p destroyed\n", this);
+}
+
+bool DAG::add(const std::string & tag,
+	      const std::string & command)
+{
+#if 0
+	Graph::Tree * tree = new Graph::Tree(tag, command);
+
+	if (!root_) {
+		root_ = tree;
+		return true;
+	}
+
+	return root_->children(tree);
+#endif
+
+	return false;
+}
+
+bool DAG::remove(const std::string & tag)
+{
+#if 0
+	if (!root_) {
+		return root_->remove(tag);
+	}
+#endif
+
+	return false;
+}
+
+std::vector<Graph::Node *> DAG::chain(std::string tag_from,
+				      std::string tag_to)
+{
+	TR_DBG("Extracting filter chain from '%s' to '%s'\n",
+	       tag_from.c_str(), tag_to.c_str());
+
+	BUG_ON(tag_from.size() == 0);
+	BUG_ON(tag_to.size() == 0);
+
+	//	Graph::Node *              node;
+	std::vector<Graph::Node *> filters;
+	//	node = dag.find(tag_out);
+
+
+	return filters;
+}
---
+++ src/filter.h
@@ -0,0 +1,97 @@
+//
+// Copyright (C) 2007, 2008 Francesco Salvestrini
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#ifndef FILTER_H
+#define FILTER_H
+
+#include "config.h"
+
+#include <string>
+#include <vector>
+
+namespace Graph {
+	// Rule11: 1 input, 1 output
+	class Rule11 {
+	public:
+		Rule11(const std::string & command);
+		~Rule11(void);
+
+		bool run(std::string & input,
+			 std::string & output);
+
+	protected:
+		Rule11(void);
+
+	private:
+		std::string command_;
+	};
+
+	class Node : public Rule11 {
+	public:
+		Node(const std::string & tag,
+		     const std::string & command);
+		~Node(void);
+
+		const std::string & tag(void);
+
+	protected:
+		Node(void);
+
+	private:
+		std::string tag_;
+	};
+
+	class Tree : public Node {
+	public:
+		Tree(const std::string & tag,
+		     const std::string & command);
+		~Tree(void);
+
+		void                        father(Tree * tree);
+		Tree *                      father(void);
+		void                        child(Tree * tree);
+		const std::vector<Tree *> & children(void);
+
+	protected:
+		Tree(void);
+
+	private:
+		Tree *              father_;
+		std::vector<Tree *> children_;
+	};
+
+	class DAG {
+	public:
+		DAG(void);
+		~DAG(void);
+
+		bool                       add(const std::string & tag,
+					       const std::string & command);
+		bool                       remove(const std::string & tag);
+
+		std::vector<Graph::Node *> chain(std::string tag_from,
+						 std::string tag_to);
+
+	protected:
+
+	private:
+		Tree * root_;
+	};
+};
+
+#endif // FILTER_H
