efe6b451ee883b64adcbdf050d9e86a8bfdbdfa7
Francesco Salvestrini <salvestrini@gmail.com>
2008-11-02 16:55:27 +0100

    Updated site index file

--- site/in/index.scm
+++ site/in/index.scm
@@ -1,10 +1,90 @@
 (h3 "DESCRIPTION")
 (p "Filter Chains Processor (FCP) is a command line utility that "
-   "handles parallel files transformations, automatically building filter "
+   "handles multiple files transformations, automatically building filter "
    "chains in order to transform each input file from its format to the "
    "desired output format.")
 (p "Each filter chain is computed dinamically, using external tools (defined "
-   "by the user in a configuration file) or internal functions.")
+   "by the user in a configuration file).")
+
+(h3 "USAGE")
+(h4 "Installation")
+(p "FCP uses the autoconf/automake facilities. The simplest way to install "
+   "the package in your system is:")
+(ul
+ (li (kbd "cd") " to the directory containing the package's source code "
+     "and type " (kbd "./configure") " to configure the package for your "
+     "system."
+     "Running " (kbd "configure") " might take a while. While running, "
+     "it prints some messages telling which features it is checking for")
+ (li "Type " (kbd "make") " to compile the package")
+ (li "Optionally, type " (kbd "make check") " to run any self-tests that "
+     "come with the package")
+ (li "Type " (kbd "make install") " to install the programs and any data "
+     "files and documentation"))
+
+(p "Use " (kbd "./configure --help") " to get a brief help of all "
+   "configuration tweaks available."
+   (br)
+   (br)
+   "You can remove the program binaries and object files from the "
+   "source code directory by typing " (kbd "make clean") ". "
+   "To also remove the files that " (samp "configure") " created (so you "
+   "can compile the package for a different architecture), type "
+   (kbd "make distclean") "."
+   (br)
+   (br)
+   "You can also type " (kbd "make uninstall") " to remove the installed "
+   "files."
+   (br)
+   (br)
+   "Note that the " (samp "--prefix") " option allows installation using "
+   "different paths than the default ones, this option enables sandbox "
+   "installations.")
+
+(h4 "Use")
+(p "")
+
+(pre (@ class "terminal")
+     "
+
+        Usage: fcp [OPTION]... [TRANSFORMATION]...
+
+        Options:
+          -r, --rules=FILE        use alternate rules file
+                                  [default $HOME/.fcp/rules]
+          -m, --max-depth=NUM     use NUM as max filter-chains depth
+                                  [default 16]
+          -t, --temp-dir=DIR      use DIR as temporary directory
+                                  [default $HOME/.fcp/cache]
+          -s, --separator=CHAR    use CHAR as INPUTFILE/OUTPUTFILE separator
+                                  [default `:']
+          -n, --dry-run           display commands without modifying any files
+          -d, --debug             enable debugging traces
+          -v, --verbose           verbosely report processing
+          -h, --help              print this help, then exit
+          -V, --version           print version number, then exit
+
+")
+
+(h5 "Options:")
+(p "Although some (maybe) sane default values are hardwired into the "
+   "program, the following options allow overriding:")
+
+(ul
+ (li (kbd "-r") ", " (kbd "--rules")       ": "
+     "Specify a different rules file")
+ (li (kbd "-m") ", " (kbd "--max-depth")   ": "
+     "Specify maximum filter chains length")
+ (li (kbd "-r") ", " (kbd "--temp-dir")    ": "
+     "Specify a temporary directory")
+ (li (kbd "-s") ", " (kbd "--separator")   ": "
+     "Overrides transformation separator")
+ (li (kbd "-n") ", " (kbd "--dry-run")     ": "
+     "Run the program without modifyng any files")
+ )
+
+(h4 "Rules file format:")
+(p "")
 
 (h3 "COPYING")
 (p "FCP is licensed under the "
