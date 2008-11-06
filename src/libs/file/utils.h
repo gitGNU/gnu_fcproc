241e8b56a46d7983f51d4442c475027ded6ab12f
Francesco Salvestrini <salvestrini@gmail.com>
2008-11-06 22:29:29 +0100

    Renamed ispresent() as exists()src/libs/file/utils.cxx

--- src/libs/file/utils.h
+++ src/libs/file/utils.h
@@ -28,7 +28,7 @@ namespace File {
 	std::string basename(const std::string & s);
 	std::string extension(const std::string & s);
 	time_t      mtime(const std::string & s);
-	bool        ispresent(const std::string & s);
+	bool        exists(const std::string & s);
 };
 
 #endif // LIBS_FILE_UTILS_H
