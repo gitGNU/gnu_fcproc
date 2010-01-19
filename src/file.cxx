8c7813b1486f3cf74119d3f909dc13f6c8346155
Francesco Salvestrini <salvestrini@gmail.com>
2010-01-19 22:37:28 +0100

    Added missing files

---
+++ src/file.cxx
@@ -0,0 +1,64 @@
+// -*- c++ -*-
+
+//
+// Copyright (C) 2008, 2009 Francesco Salvestrini
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#include "config.h"
+
+#include <string>
+#include <boost/filesystem.hpp>
+
+#include "libs/misc/debug.h"
+#include "file.h"
+
+namespace fcp {
+
+        file::file()
+        { }
+
+        file & file::operator=(const file & rhs)
+        {
+                path_ = rhs.path_;
+                type_ = rhs.type_;
+        }
+
+        file::file(const boost::filesystem::path & path,
+                   const std::string &             type) :
+                path_(path),
+                type_(type)
+        {
+                if (type.empty()) {
+                        std::string tmp = boost::filesystem::extension(path);
+                        type_ = tmp.substr(1);
+                }
+
+                TR_DBG("File: name = '%s' / type = '%s'\n",
+                       path_.string().c_str(),
+                       type_.c_str());
+        }
+
+        file::~file()
+        { }
+
+        const boost::filesystem::path & file::path() const
+        { return path_; }
+
+        const std::string & file::type() const
+        { return type_; }
+
+};
